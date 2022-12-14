0a1db4ec44f693481e9d3381d0c42a69f5f78229
Hendrik Näther <hendrik.naether@softwareag.com>
2022-12-14 14:57:16 +0100

    Upped WebSDK version to 1015.0.235

---
+++ app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule as ngRouterModule } from '@angular/router';
+import { CoreModule, BootstrapComponent, RouterModule } from '@c8y/ngx-components';
+import { CustomizationExampleModule } from './src/customization-example/customization-example.module';
+import { LocalDataGridModule } from './src/local-data-example/local-data-grid.module';
+import { MinimumDataGridModule } from './src/minimum-example/minimum-data-grid.module';
+import { QueryPlaygrounModule } from './src/query-playground/query-playground.module';
+import { RemoteDataGridModule } from './src/remote-data-example/remote-data-grid.module';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    RouterModule.forRoot(),
+    ngRouterModule.forRoot([], { enableTracing: false, useHash: true }),
+    CoreModule.forRoot(),
+
+    LocalDataGridModule,
+    MinimumDataGridModule,
+    CustomizationExampleModule,
+    RemoteDataGridModule,
+    QueryPlaygrounModule
+
+  ],
+  bootstrap: [BootstrapComponent]
+})
+export class AppModule {}
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import './polyfills';
+import './i18n';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './src/app.module';
+import { AppModule } from './app.module';
 
 declare const __MODE__: string;
 if (__MODE__ === 'production') {
@@ -11,6 +12,5 @@ if (__MODE__ === 'production') {
 
 export function bootstrap() {
   return platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch((err) => console.log(err));
+    .bootstrapModule(AppModule).catch((err) => console.log(err));
 }
